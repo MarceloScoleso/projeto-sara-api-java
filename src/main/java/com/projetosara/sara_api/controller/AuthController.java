8bc2bef06e0aa120713beb9dafcc8ce7ae9fc472
MarceloScoleso <marcelo.scolesojr@gmail.com>
2025-06-08 03:32:10 -0300

    schema

--- src/main/java/com/projetosara/sara_api/controller/AuthController.java
+++ src/main/java/com/projetosara/sara_api/controller/AuthController.java
@@ -3,6 +3,11 @@ package com.projetosara.sara_api.controller;
 import com.projetosara.sara_api.dto.LoginRequestDTO;
 import com.projetosara.sara_api.security.JwtUtil;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.*;
@@ -21,22 +26,39 @@ public class AuthController {
     private final JwtUtil jwtUtil;
     private final UserDetailsService userDetailsService;
 
+    @Operation(
+        summary = "Realiza o login do usuário",
+        requestBody = @RequestBody(
+            required = true,
+            content = @Content(
+                schema = @Schema(implementation = LoginRequestDTO.class),
+                examples = @ExampleObject(
+                    name = "Exemplo de Login",
+                    value = """
+                        {
+                        "username": "Marcelo@email.com",
+                        "password": "Marcio12"
+                        }
+                        """
+                )
+            )
+        )
+    )
     @PostMapping("/login")
-public ResponseEntity<?> login(@Valid @RequestBody LoginRequestDTO request) {
-    try {
-        authManager.authenticate(
-            new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword())
-        );
+    public ResponseEntity<?> login(@Valid @org.springframework.web.bind.annotation.RequestBody LoginRequestDTO request) {
+        try {
+            authManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword())
+            );
 
-        UserDetails user = userDetailsService.loadUserByUsername(request.getUsername());
-        String token = jwtUtil.generateToken(user);
+            UserDetails user = userDetailsService.loadUserByUsername(request.getUsername());
+            String token = jwtUtil.generateToken(user);
 
-        return ResponseEntity.ok(new AuthResponse(token));
-    } catch (AuthenticationException e) {
-        return ResponseEntity.status(401).body("Usuário ou senha inválidos");
+            return ResponseEntity.ok(new AuthResponse(token));
+        } catch (AuthenticationException e) {
+            return ResponseEntity.status(401).body("Usuário ou senha inválidos");
+        }
     }
-}
 
-
-public record AuthResponse(String token) {}
+    public record AuthResponse(String token) {}
 }
